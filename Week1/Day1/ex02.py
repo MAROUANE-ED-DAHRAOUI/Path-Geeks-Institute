b298537435e57e1a061f1711e289ccb451364f48
MAROUANE-ED-DAHRAOUI <eddahraouimarouane@gmail.com>
2025-06-18 11:45:30 +0100

    Python Exercises Week1-Day1

---
+++ Week1/Day1/ex02.py
@@ -0,0 +1,2 @@
+Sum_Number = (99**3) * 8
+print(Sum_Number)
---
+++ Week1/Day1/ex03.py
@@ -0,0 +1,8 @@
+MyName = "Marouane"
+Name = input("You bro you can enter your name : ")
+print(Name)
+
+if MyName == Name:
+    print("you have the same name Ohh lala :-)")
+else:
+    print("You have Great Name :-)")
