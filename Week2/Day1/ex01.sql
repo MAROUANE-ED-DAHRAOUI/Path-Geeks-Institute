5c3cff1493ecf78065967ea8d2e9d90604c98824
MAROUANE-ED-DAHRAOUI <eddahraouimarouane@gmail.com>
2025-06-26 13:14:23 +0100

    Finish

---
+++ Week2/Day1/ex01.sql
@@ -0,0 +1,34 @@
+-- insert into items(name_item,price)
+-- values('Small Desk', 100) ,
+-- 	  ('Large desk', 300) ,
+-- 	  ('Fan', 80)
+
+-- DELETE FROM items WHERE id_item in(4, 5, 6, 7, 8, 9)
+-- SELECT * FROM items;
+
+-- insert into customers(name_customer, lastname_customer)
+-- values('Greg', 'Jones'),
+-- ('Sandra', 'Jones'),
+-- ('Scott', 'Scott'),
+-- ('Trevor', 'Green'),
+-- ('Melanie', 'Johnson')
+
+-- SELECT * FROM customers;
+
+-- SELECT * FROM items WHERE price > 80
+
+-- SELECT * FROM items WHERE price < 300
+
+-- SELECT * FROM customers WHERE  lastname_customer = 'Smith'
+
+-- SELECT * FROM customers WHERE  lastname_customer = 'Jones'
+
+-- SELECT * from customers WHERE name_customer != 'Scott'
+
+
+
+
+
+
+
+
