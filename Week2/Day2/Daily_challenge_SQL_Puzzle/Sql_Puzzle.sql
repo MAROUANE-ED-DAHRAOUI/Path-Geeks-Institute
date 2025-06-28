2dbbddc545826eb6a5a76967fc04c6eee54e5e8f
MAROUANE-ED-DAHRAOUI <eddahraouimarouane@gmail.com>
2025-06-29 00:27:04 +0100

    UPD

---
+++ Week2/Day2/Daily_challenge_SQL_Puzzle/Sql_Puzzle.sql
@@ -0,0 +1,15 @@
+-- Q1. What will be the OUTPUT of the following statement?
+
+output : COUNT = 0
+
+-- Q2. What will be the OUTPUT of the following statement?
+
+output : count = 1
+
+--Q3. What will be the OUTPUT of the following statement?
+
+output : count = 1
+
+--Q4. What will be the OUTPUT of the following statement?
+
+output : count = 1
