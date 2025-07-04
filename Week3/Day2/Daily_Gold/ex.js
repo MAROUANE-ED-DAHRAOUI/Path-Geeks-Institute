f48ad548822bd71c48041bd7207b46f1c872225f
MAROUANE-ED-DAHRAOUI <eddahraouimarouane@gmail.com>
2025-07-04 19:55:12 +0100

    Is good exercise

---
+++ Week3/Day2/Daily_Gold/ex.js
@@ -0,0 +1,13 @@
+const numbers = [5,0,9,1,7,4,2,6,3,8];
+
+//1-> Using the .toString() method convert the array to a string.
+const strings = numbers.map(number => number.toString());
+
+// 2-> Using the .join()method convert the array to a string. Try passing different values into the join.
+const joinedStr = strings.join(', ');
+console.log(joinedStr);
+
+
+// 3-> 
+
+
