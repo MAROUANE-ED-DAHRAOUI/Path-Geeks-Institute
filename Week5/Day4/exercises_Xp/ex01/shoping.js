a1d7600e881f574b71292fe26d4bc5604fd46a00
MAROUANE ED DAHRAOUI <eddahraouimarouane@gmail.com>
2025-08-03 10:33:32 +0100

    Add ex01 finish

---
+++ Week5/Day4/exercises_Xp/ex01/shoping.js
@@ -0,0 +1,15 @@
+const products = require("./products");
+
+//function pour trouver un produit par son name
+function findProductByName(productName) {
+  const product = products.find((item) => item.name === productName);
+  if (product) {
+    console.log(
+      `Product found: ${product.name}, Price: ${product.price}, Category: ${product.category}`
+    );
+  } else {
+    console.log("Product not found");
+  }
+}
+findProductByName("Laptop");
+findProductByName("Shoes");
