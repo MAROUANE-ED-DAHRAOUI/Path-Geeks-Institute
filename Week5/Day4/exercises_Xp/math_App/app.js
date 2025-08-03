4befdefcac7a110d533b8dffe50a69f616390e46
MAROUANE ED DAHRAOUI <eddahraouimarouane@gmail.com>
2025-08-03 10:44:41 +0100

    Creating and Using a Custom Module

---
+++ Week5/Day4/exercises_Xp/math_App/app.js
@@ -0,0 +1,8 @@
+const _ = require("lodash");
+const math = require("./math.js");
+
+const sum = math.Addition(5, 3);
+const product = math.Multiplication(4, 6);
+
+console.log("Sum:", sum);
+console.log("Product:", product);
