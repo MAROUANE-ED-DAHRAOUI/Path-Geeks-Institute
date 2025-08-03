0b736a6ce74405e247403b6ba6e496f017180a8b
MAROUANE ED DAHRAOUI <eddahraouimarouane@gmail.com>
2025-08-03 10:37:06 +0100

    Advanced Module Usage using ES6 module syntax

---
+++ Week5/Day4/exercises_Xp/ex02/data.js
@@ -0,0 +1,31 @@
+const personnes = [
+  {
+    name: "Marouane ed-dahraoui",
+    age: 26,
+    location: "Sale",
+  },
+  {
+    name: "khalid El Amrani",
+    age: 25,
+    location: "Casablanca",
+  },
+  { name: "yassine bnasnas", age: 27 },
+  {
+    name: "yossef walo",
+    age: 28,
+    location: "Rabat",
+  },
+  {
+    name: "Hicham smati",
+    age: 30,
+    location: "sawira",
+  },
+  {
+    name: "rachid houminat",
+    age: 29,
+    location: "agadir",
+  },
+];
+
+// Export the array :
+module.exports = personnes;
